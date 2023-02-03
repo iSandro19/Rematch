a1ddb0074dbf2fc0872fc119aeb25dfd3420fb5f
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-02-03 09:53:22 +0100

    Update pixel_runner.py

--- playground/sandro/pixel_runner.py
+++ playground/sandro/pixel_runner.py
@@ -49,8 +49,8 @@ def player_animation():
         player_surface = player_jump_surface
     else:
         player_index += 0.1
-        if player_index >= len(player_walk_surface): player_index = 0
-        player_surface = player_walk_surface[int(player_index)]
+        if player_index >= len(player_walk_index): player_index = 0
+        player_surface = player_walk_index[int(player_index)]
 
 # Initialize pygame
 pygame.init()
@@ -70,18 +70,30 @@ clock = pygame.time.Clock()
 sky_surface = pygame.image.load("graphics/sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
+# Snail
+snail_frame1_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_frame2_surface = pygame.image.load("graphics/snail/snail2.png").convert_alpha()
+snail_frame_index = [snail_frame1_surface, snail_frame2_surface]
+snail_index = 0
+snail_surface = snail_frame_index[snail_index]
+
+# Fly
+fly_frame1_surface = pygame.image.load("graphics/fly/fly1.png").convert_alpha()
+fly_frame2_surface = pygame.image.load("graphics/fly/fly2.png").convert_alpha()
+fly_frame_index = [fly_frame1_surface, fly_frame2_surface]
+fly_index = 0
+fly_surface = fly_frame_index[fly_index]
+
 # Obstacles
-snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-fly_surface = pygame.image.load("graphics/fly/fly1.png").convert_alpha()
 obstacles_rectangle_list = []
 
 # Player
 player_walk1_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_walk2_surface = pygame.image.load("graphics/player/player_walk_2.png").convert_alpha()
-player_walk_surface = [player_walk1_surface, player_walk2_surface]
+player_walk_index = [player_walk1_surface, player_walk2_surface]
 player_index = 0
 player_jump_surface = pygame.image.load("graphics/player/player_jump.png").convert_alpha()
-player_surface = player_walk_surface[player_index]
+player_surface = player_walk_index[player_index]
 player_rectangle = player_surface.get_rect(midbottom = (80, 300))
 
 # Player stand
@@ -101,9 +113,15 @@ game_name_rectangle = game_name.get_rect(center = (400, 75))
 game_message = text_font.render("Press space to start", False, (10, 10, 10))
 game_message_rectangle = game_message.get_rect(center = (400, 320))
 
-# Timer
+# Timers
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1300)
+
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer, 500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 200)
 
 # Game loop
 while True:
@@ -134,6 +152,16 @@ while True:
                     obstacles_rectangle_list.append(fly_surface.get_rect(midbottom = (randint(900, 1100), 200)))
                 else:
                     obstacles_rectangle_list.append(snail_surface.get_rect(midbottom = (randint(900, 1100), 300)))
+            # Snail animation timer
+            if event.type == snail_animation_timer:
+                if snail_index == 0: snail_index = 1
+                else: snail_index = 0
+                snail_surface = snail_frame_index[snail_index]
+            # Fly animation timer
+            if event.type == fly_animation_timer:
+                if fly_index == 0: fly_index = 1
+                else: fly_index = 0
+                fly_surface = fly_frame_index[fly_index]
         # Game over screen
         elif game_state == 2:
             # Go to game screen
